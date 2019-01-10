61d3669ce2ba57c7f1afbe9ba6495025558dc12a
ycdfwzy <ycdfwzy@outlook.com>
2019-01-10 23:59:34 +0800

    modify

--- testcases/AVLtree.c
+++ testcases/AVLtree.c
@@ -13,62 +13,80 @@ struct AVLNode nodes[10000];
 int avai = 1;
 
 int max(int a, int b){
-	if(a > b)
+	if(a > b) {
 		return a;
-	else
-		return b;
+	}
+	return b;
 }
 
 void initTree(){
+	int i;
 	root = 0;
-	for(int i = 0; i < 100; ++i){
+	for(i = 0; i < 100; i=i+1){
 		nodes[i].is_null = 1;
 		nodes[i].height = 0;
 		nodes[i].left = 0;
 		nodes[i].right = 0;
 	}
+	return;
 }
 
 int getHeight(int node){
-	if(!nodes[node].is_null)
-		return nodes[node].height;
-	return -1;
+	int ret;
+	if (!nodes[node].is_null) {
+		ret = nodes[node].height;
+		return ret;
+	}
+	ret = -1;
+	return ret;
 }
 
 int leftLeftRotation(int node){
-	int temp = nodes[node].left;
+	int temp = nodes[node].left, p1, p2;
 	nodes[node].left = nodes[nodes[node].left].right;
 	nodes[temp].right = node;
 
-	nodes[node].height = max(getHeight(nodes[node].left), getHeight(nodes[node].right)) + 1;
-	nodes[temp].height = max(getHeight(nodes[temp].left), getHeight(nodes[temp].right)) + 1;
+	p1 = getHeight(nodes[node].left);
+	p2 = getHeight(nodes[node].right);
+	nodes[node].height = max(p1, p2) + 1;
+	p1 = getHeight(nodes[temp].left);
+	p2 = getHeight(nodes[temp].right);
+	nodes[temp].height = max(p1, p2) + 1;
 	return temp;
 }
 
 int rightRightRotation(int node){
-	int temp = nodes[node].right;
+	int temp = nodes[node].right, p1, p2;
 	nodes[node].right = nodes[nodes[node].right].left;
 	nodes[temp].left = node;
 
-	nodes[node].height = max(getHeight(nodes[node].left), getHeight(nodes[node].right)) + 1;
-	nodes[temp].height = max(getHeight(nodes[temp].left), getHeight(nodes[temp].right)) + 1;
+	p1 = getHeight(nodes[node].left);
+	p2 = getHeight(nodes[node].right);
+	nodes[node].height = max(p1, p2) + 1;
+	p1 = getHeight(nodes[temp].left);
+	p2 = getHeight(nodes[temp].right);
+	nodes[temp].height = max(p1, p2) + 1;
 	return temp;
 }
 
 int leftRightRotation(int node){
+	int ret;
 	nodes[node].left = rightRightRotation(nodes[node].left);
-	return leftLeftRotation(node);
+	ret = leftLeftRotation(node);
+	return ret;
 }
 
 int rightLeftRotation(int node){
+	int ret;
 	nodes[node].right = leftLeftRotation(nodes[node].right);
-	return rightRightRotation(node);
+	ret = rightRightRotation(node);
+	return ret;
 }
 
 int insert(int node, int elem){
 	if(nodes[node].is_null){
 		int i = avai;
-		while(!nodes[i].is_null)i = i + 1;
+		while(!nodes[i].is_null) {i = i + 1;}
 		nodes[i].is_null = 0;
 		nodes[i].elem = elem;
 		avai = i + 1;
@@ -77,24 +95,28 @@ int insert(int node, int elem){
 		nodes[node].left = insert(nodes[node].left, elem);
 		nodes[node].height = max(nodes[nodes[node].left].height, nodes[nodes[node].right].height) + 1;
 		if (getHeight(nodes[node].left) - getHeight(nodes[node].right) == 2){
-			if (elem < nodes[nodes[node].left].elem)
+			if (elem < nodes[nodes[node].left].elem) {
 				node = leftLeftRotation(node);
-			else
+			}
+			else {
 				node = leftRightRotation(node);
+			}
 		}
 		return node;
 	}else if(elem > nodes[node].elem){
 		nodes[node].right = insert(nodes[node].right, elem);
 		nodes[node].height = max(nodes[nodes[node].left].height, nodes[nodes[node].right].height) + 1;
 		if (getHeight(nodes[node].left) - getHeight(nodes[node].right) == -2){
-			if (elem > nodes[nodes[node].right].elem)
+			if (elem > nodes[nodes[node].right].elem) {
 				node = rightRightRotation(node);
-			else
+			}
+			else {
 				node = rightLeftRotation(node);
+			}
 		}
 		return node;
-	}else
-		return 0;
+	}
+	return 0;
 }
 
 void addNode(int elem){
@@ -102,24 +124,33 @@ void addNode(int elem){
 }
 
 int search(int node, int elem){
-	if(nodes[node].is_null)
+	int ret;
+	if(nodes[node].is_null) {
 		return 0;
-	if(elem == nodes[node].elem)
+	}
+	if(elem == nodes[node].elem) {
 		return node;
-	else if(elem < nodes[node].elem)
-		return search(nodes[node].left, elem);
-	else
-		return search(nodes[node].right, elem);
+	}
+	else if(elem < nodes[node].elem) {
+		ret = search(nodes[node].left, elem);
+		return ret;
+	}
+	else {
+		ret = search(nodes[node].right, elem);
+		return ret;
+	}
 }
 
 int searchNode(int elem){
-	return search(root, elem);
+	int ret;
+	ret = search(root, elem);
+	return ret;
 }
 
 
 
 void removeNode(int elem){
-
+	return;
 }
 
 void printNode(int node){
@@ -128,23 +159,30 @@ void printNode(int node){
 		return;
 	}
 	printf("%d left child is ", nodes[node].elem);
-	if(nodes[nodes[node].left].is_null)
+	if(nodes[nodes[node].left].is_null) {
 		printf("NULL, right child is ");
-	else
+	}
+	else {
 		printf("%d, right child is ", nodes[nodes[node].left].elem);
+	}
 
-	if(nodes[nodes[node].right].is_null)
+	if(nodes[nodes[node].right].is_null) {
 		printf("NULL\n");
-	else
+	}
+	else{
 		printf("%d\n", nodes[nodes[node].right].elem);
+	}
+	return;
 }
 
 void printAVL(int node){
-	if(nodes[node].is_null)
+	if(nodes[node].is_null) {
 		return;
+	}
 	printNode(node);
     printAVL(nodes[node].left);
     printAVL(nodes[node].right);
+    return;
 }
 
 int main(){
@@ -161,8 +199,9 @@ int main(){
 			removeNode(elem);
 			printAVL(root);
 		}
-		else if(comm == 2)
+		else if(comm == 2) {
 			printNode(searchNode(elem));
+		}
 		
 	}
 	return 0;
